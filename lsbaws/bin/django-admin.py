00c54b2c78107e52886a255caae35c9ed3342a0b
frantz crozilhac <fran.stacksta@gmail.com>
2021-03-04 16:23:55 +0100

    done

---
+++ lsbaws/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/utilisateur/code/Narf-stack/webserver-from-scratch/lsbaws/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
